06cacdba9343abdc847b396655532283f74a8ab1
wuzheng <17173as@163.com>
2019-09-11 22:10:27 +0800

    增加爬虫代码

---
+++ com/hz/yk/crawler/maoyan/main.py
@@ -0,0 +1,50 @@
+import json
+import re
+
+import requests
+
+
+def get_one_page(url):
+    headers = {
+        'User-Agent': 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    return None
+
+
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score': item[5] + item[6]
+        }
+
+
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+
+
+def main():
+    url = 'http://maoyan.com/board/4'
+    html = get_one_page(url)
+    print(html)
+
+    for item in parse_one_page(html):
+        write_to_file(item)
+        print(item)
+
+
+if __name__ == '__main__':
+    main()
--- com/hz/yk/re/re3.py
+++
@@ -1,4 +0,0 @@
-def get_one_page(url):
-    headers = {
-
-    }

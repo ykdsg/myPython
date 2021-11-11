dad879239be46ba20c5926374b3553ff3973354e
wuzheng <17173as@163.com>
2021-11-11 09:49:13 +0800

    回溯算法增加正则匹配的例子

--- geekbang/algo/39_back_track/regex_01.py
+++ geekbang/algo/39_back_track/regex_01.py
@@ -1,33 +1,34 @@
 is_match = False
 
-# 正则表达式，"*"匹配任意多个(大于等于 0 个)任意字符,"?"匹配零个或者一个任意字符
-def rmatch(regex_index: int, str_index: int, regex: str, main_str: str):
+
+def rmatch(regex_index, str_index, regex, main_str):
     global is_match
     if is_match:
         return
 
-    # 说明正则表达式全部匹配
+    # 表示能够匹配
     if regex_index >= len(regex):
         is_match = True
         return
-    # 主字符串已经没得匹配了，但是正则还没匹配完
-    if str_index >= len(main_str) and regex_index < len(regex):
+
+    # 表示没能匹配成功
+    if regex_index < len(regex) and str_index >= len(main_str):
         is_match = False
         return
 
-    # 匹配0个或多个字符的情况
     if regex[regex_index] == '*':
         for i in range(str_index, len(main_str)):
+            # '*'匹配0个或者多个字符 
             rmatch(regex_index + 1, i, regex, main_str)
-    elif regex[regex_index] == '?':
-        # 匹配0个字符和1个字符的情况
+
+    if regex[regex_index] == '?':
+        # '?'匹配0个或1个字符
         rmatch(regex_index + 1, str_index, regex, main_str)
         rmatch(regex_index + 1, str_index + 1, regex, main_str)
-    else:
-        if regex[regex_index] == main_str[str_index]:
-            rmatch(regex_index + 1, str_index + 1, regex, main_str)
 
-    pass
+    # 当期字符匹配
+    if regex[regex_index] == main_str[str_index]:
+        rmatch(regex_index + 1, str_index + 1, regex, main_str)
 
 
 if __name__ == '__main__':
@@ -48,3 +49,5 @@ if __name__ == '__main__':
     main = 'abceefecd'
     rmatch(0, 0, regex, main)
     print(is_match)
+
+#     最终结果应该是True,True,False

d9d35c6b3cdfa47cc3aa16fb75b24ed016f4fd82
wuzheng <17173as@163.com>
2021-11-12 19:25:57 +0800

    动态回归简单的demo 0-1背包问题。使用1维数组解决。

--- geekbang/algo/40_dynamic_programming/01_bag_single.py
+++ geekbang/algo/40_dynamic_programming/01_bag_single.py
@@ -1,3 +1,6 @@
+# 使用1维数组来实现动态规划
+
+
 def check_weight(items: list, max_weight: int) -> int:
     """
     固定容量的背包，计算能装进背包的物品组合的最大重量
@@ -8,24 +11,21 @@ def check_weight(items: list, max_weight: int) -> int:
     """
     item_count = len(items)
     # max_weight + 1，因为最开始前面有个0，0~max_weight，实际上是max_weight + 1个
-    memo = [[-1] * (max_weight + 1) for i in range(item_count)]
+    memo = [-1] * (max_weight + 1)
     # 第一行数据需要特殊处理
-    memo[0][0] = 1
-    memo[0][items[0]] = 1
+    memo[0] = 1
+    memo[items[0]] = 1
 
     for i in range(1, item_count):
-        # 不把第i个物品放入的时候，相当于直接继承上一层的数据
-        for j in range(max_weight + 1):
-            if memo[i - 1][j] == 1:
-                memo[i][j] = 1
+        # 这里就不用考虑第i个物品不放的情况，因为1维的数组相当于已经继承了。
         # 将第i个物品放入的时候，可能存在的情况，这里需要+1，因为rang 不包括最后一位
         # 这里处理了下，因为最终不可以超过max_weight ，只考察 max_weight - items[i] 之前的数据才有意义
         for j in range(max_weight - items[i] + 1):
-            if memo[i - 1][j] == 1:
-                memo[i][j + items[i]] = 1
+            if memo[j] == 1:
+                memo[j + items[i]] = 1
 
     for i in range(max_weight, -1, -1):
-        if memo[item_count - 1][i] == 1:
+        if memo[i] == 1:
             return i
 
     return 0
